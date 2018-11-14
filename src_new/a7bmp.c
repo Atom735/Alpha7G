80196c7ed13395c67698c43256480fc642996692
Atom735 <Atom735.ag47@gmail.com>
2018-11-14 21:06:05 +0300

    draw tabs

--- src_new/a7bmp.c
+++ src_new/a7bmp.c
@@ -13,6 +13,11 @@ typedef struct _S7BMP {
     /* Поля для GDI текстуры */
     HDC     hDC;
     HBITMAP hBMP;
+    /* Поля для глифа */
+    INT     nAdvance;
+    INT     nLeft;
+    INT     nTop;
+
 } S7BMP;
 
 enum {
@@ -25,6 +30,8 @@ enum {
     /* GDI карты */
     D7BMP_GDI_RGB,
     D7BMP_GDI_RGBA,
+    /* Glyph Alpha карта */
+    D7BMP_GLYPH,
 };
 
 /* Создание новой текстуры */
@@ -51,6 +58,7 @@ S7BMP *A7BmpCreate ( UINT iType, UINT nWidth, UINT nHeight, UINT nStride ) {
         case D7BMP_A7_RGBA:     bMemData = TRUE;    break;
         case D7BMP_GDI_RGB:     bMemData = FALSE;   break;
         case D7BMP_GDI_RGBA:    bMemData = FALSE;   break;
+        case D7BMP_GLYPH:       bMemData = FALSE;   break;
     }
 
     S7BMP *o = ( S7BMP* ) malloc ( bMemData ? sizeof ( S7BMP ) + ( nStride * nHeight ) : sizeof ( S7BMP ) );
@@ -60,8 +68,6 @@ S7BMP *A7BmpCreate ( UINT iType, UINT nWidth, UINT nHeight, UINT nStride ) {
     o -> nHeight = nHeight;
     o -> nStride = nStride;
     o -> pData   = ( BYTE* ) ( o ) + sizeof ( S7BMP );
-    // o -> hDC     = NULL;
-    // o -> hBMP    = NULL;
     return o;
 }
 /* Освобождение ресурсов текстуры */
@@ -118,6 +124,8 @@ S7BMP *A7BmpCreateByGDI ( HDC hDC, UINT nWidth, UINT nHeight, BOOL bAlpha, BOOL 
     return o;
 }
 
+
+
 /* Копирование части текстуры в другую текстуру */
 VOID A7BmpCopy ( S7BMP * pDst, UINT nDX, UINT nDY, S7BMP * pSrc, UINT nSX, UINT nSY, UINT nW, UINT nH ) {
     UINT iBppDT = 0;
@@ -128,6 +136,7 @@ VOID A7BmpCopy ( S7BMP * pDst, UINT nDX, UINT nDY, S7BMP * pSrc, UINT nSX, UINT 
         case D7BMP_A7_RGBA:  iBppDT = 4; break;
         case D7BMP_GDI_RGB:  iBppDT = 3; break;
         case D7BMP_GDI_RGBA: iBppDT = 4; break;
+        case D7BMP_GLYPH:    iBppDT = 1; break;
     }
     UINT iBppST = 0;
     switch ( pDst -> iType ) {
@@ -137,6 +146,7 @@ VOID A7BmpCopy ( S7BMP * pDst, UINT nDX, UINT nDY, S7BMP * pSrc, UINT nSX, UINT 
         case D7BMP_A7_RGBA:  iBppST = 4; break;
         case D7BMP_GDI_RGB:  iBppST = 3; break;
         case D7BMP_GDI_RGBA: iBppST = 4; break;
+        case D7BMP_GLYPH:    iBppST = 1; break;
     }
     /* Если подобная / одинаковая структура */
     if ( iBppDT == iBppST ) {
@@ -145,6 +155,7 @@ VOID A7BmpCopy ( S7BMP * pDst, UINT nDX, UINT nDY, S7BMP * pSrc, UINT nSX, UINT 
         }
     }
 }
+
 
 /* Рисует GDI текстуру на контекст */
 VOID A7BmpGDI_Draw ( HDC hDC, UINT nDX, UINT nDY, S7BMP * pSrc, UINT nSX, UINT nSY, UINT nW, UINT nH ) {
@@ -165,3 +176,101 @@ VOID A7BmpGDI_Draw ( HDC hDC, UINT nDX, UINT nDY, S7BMP * pSrc, UINT nSX, UINT n
     }
 
 }
+
+
+/* Генерация маски вкладки */
+S7BMP *A7BmpGen_Tab ( CONST UINT nH, CONST FLOAT _an, CONST FLOAT _r0, CONST FLOAT _r1 ) {
+
+    // CONST UINT nH = 768;
+
+    // CONST FLOAT _an = 1.0f;
+    CONST FLOAT _cx = cosf ( _an );
+    CONST FLOAT _cy = sinf ( _an );
+
+    // CONST FLOAT _r0 = 333.0f;
+    CONST FLOAT _x0 = 0.0f;
+    CONST FLOAT _y0 = _r0;
+
+    CONST FLOAT _xa = _x0 + _r0 * _cy;
+    CONST FLOAT _ya = _y0 - _r0 * _cx;
+    CONST FLOAT _ca = _cx * _xa + _cy * _ya;
+
+    // CONST FLOAT _r1 = 123.0f;
+    CONST FLOAT _y1 = (FLOAT)nH-_r1;
+
+    CONST FLOAT _yb = _y1 + _r1 * _cx;
+    CONST FLOAT _xb = ( _yb - _ya ) / _cy * _cx + _xa;
+
+    CONST FLOAT _x1 = _xb + _r1 * _cy;
+    CONST FLOAT _cb = _cx * _xb + _cy * _yb;
+
+    CONST FLOAT _cc = _cx * _yb - _cy * _xb;
+
+    CONST UINT nW = (UINT)_x1;
+
+
+
+    S7BMP *o = A7BmpCreate ( D7BMP_A7_R, nW, nH, 0 );
+    BYTE *pb = ( BYTE* ) o -> pData;
+    for ( UINT iy = 0; iy < nH; ++iy ) {
+        for ( UINT ix = 0; ix < nW; ++ix ) {
+            CONST UINT i = iy * o -> nStride + ix;
+            CONST FLOAT x = (FLOAT)(ix)+0.5f;
+            CONST FLOAT y = (FLOAT)(nH-iy)-0.5f;
+
+
+            CONST FLOAT la = _cx * x + _cy * y;
+
+            /* Segment 1 - Circle [0] */
+            if ( la < _ca ) {
+                CONST FLOAT x0 = x - _x0;
+                CONST FLOAT y0 = y - _y0;
+                CONST FLOAT r0 = sqrtf ( x0 * x0 + y0 * y0 ) - _r0 + 0.5f;
+                pb [ i ] = r0 < 0.0f ? 0x00 : r0 < 1.0f ? r0 * 255.0f : 0xff;
+            } else
+            /* Segment 3 - Circle [1] */
+            if ( la > _cb ) {
+                CONST FLOAT x0 = x - _x1;
+                CONST FLOAT y0 = y - _y1;
+                CONST FLOAT r0 = _r1 - sqrtf ( x0 * x0 + y0 * y0 ) + 0.5f;
+                pb [ i ] = r0 < 0.0f ? 0x00 : r0 < 1.0f ? r0 * 255.0f : 0xff;
+            } else
+            /* Segment 2 - Line */
+            {
+                CONST FLOAT lb = _cy * x - _cx * y + _cc + 0.5f;
+                pb [ i ] = lb < 0.0f ? 0x00 : lb < 1.0f ? lb * 255.0f : 0xff;
+
+            }
+        }
+    }
+    return o;
+}
+/* Генерация маски символа */
+S7BMP *A7BmpLoad_Symbol ( S7BMP *o, FT_Face face, UINT symbol, UINT h, UINT x, UINT y ) {
+    FT_Set_Char_Size ( face, 0, h, 96, 96 );
+    FT_Vector pen = { .x = x, .y =-y, };
+    FT_Set_Transform ( face, NULL, &pen );
+    int err = FT_Load_Char ( face, symbol, FT_LOAD_RENDER );
+    if ( err ) return NULL;
+    FT_Bitmap *bmp = &face -> glyph -> bitmap;
+    if ( o == NULL ) {
+        o = A7BmpCreate ( D7BMP_GLYPH, bmp -> width, bmp -> rows, bmp -> pitch );
+    } else {
+        o -> nWidth     = bmp -> width;
+        o -> nHeight    = bmp -> rows;
+        o -> nStride    = bmp -> pitch;
+    }
+    o -> pData      = bmp -> buffer;
+    o -> nAdvance   = face -> glyph -> advance.x;
+    o -> nLeft      = face -> glyph -> bitmap_left;
+    o -> nTop       =-face -> glyph -> bitmap_top;
+    return o;
+}
+
+
+
+
+
+
+
+
--- src_new/a7main.c
+++ src_new/a7main.c
@@ -11,8 +11,81 @@
 
 #include <JpegLib.h>
 
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
 #include "a7err.c"
 #include "a7bmp.c"
+
+/* Рисует маску с указаным цветом на цветовую карту */
+VOID A7BmpDrawAlphaMap ( S7BMP *pDst, S7BMP *pSrc, BYTE r, BYTE g, BYTE b, BYTE a, BOOL bMirror ) {
+    BYTE *pbd = ( BYTE* ) pDst -> pData;
+    BYTE *pbs = ( BYTE* ) pSrc -> pData;
+    for ( UINT iy = 0; iy < pSrc -> nHeight; ++iy ) {
+        CONST UINT idy = ( iy + pSrc -> nTop );
+        if ( idy >= pDst -> nHeight ) break;
+        for ( UINT ix = 0; ix < pSrc -> nWidth; ++ix ) {
+            CONST UINT idx = ( ix + pSrc -> nLeft );
+            if ( idx >= pDst -> nWidth ) break;
+            CONST UINT id = idy * pDst -> nStride + idx * 4;
+            CONST UINT is = ( iy ) * pSrc -> nStride + ( bMirror ? pSrc -> nWidth - ix - 1 : ix );
+            pbd [ id + 0 ] = ( pbd [ id + 0 ] * ( 0xff - pbs [ is ] ) + b * pbs [ is ] ) / 0xff;
+            pbd [ id + 1 ] = ( pbd [ id + 1 ] * ( 0xff - pbs [ is ] ) + g * pbs [ is ] ) / 0xff;
+            pbd [ id + 2 ] = ( pbd [ id + 2 ] * ( 0xff - pbs [ is ] ) + r * pbs [ is ] ) / 0xff;
+            pbd [ id + 3 ] = ( pbd [ id + 3 ] * ( 0xff - pbs [ is ] ) + a * pbs [ is ] ) / 0xff;
+        }
+    }
+}
+/* Рисует прямоугольник с указаным цветом на цветовую карту */
+VOID A7BmpDrawRect ( S7BMP *pDst, UINT nX, UINT nY, UINT nW, UINT nH, BYTE r, BYTE g, BYTE b, BYTE a ) {
+    BYTE *pbd = ( BYTE* ) pDst -> pData;
+    for ( UINT iy = 0; iy < nH; ++iy ) {
+        CONST UINT idy = ( iy + nY );
+        if ( idy >= pDst -> nHeight ) break;
+        for ( UINT ix = 0; ix < nW; ++ix ) {
+            CONST UINT idx = ( ix + nX );
+            if ( idx >= pDst -> nWidth ) break;
+            CONST UINT id = idy * pDst -> nStride + idx * 4;
+            pbd [ id + 0 ] = ( pbd [ id + 0 ] * ( 0xff - a ) + b * a ) / 0xff;
+            pbd [ id + 1 ] = ( pbd [ id + 1 ] * ( 0xff - a ) + g * a ) / 0xff;
+            pbd [ id + 2 ] = ( pbd [ id + 2 ] * ( 0xff - a ) + r * a ) / 0xff;
+            pbd [ id + 3 ] = ( pbd [ id + 3 ] * ( 0xff - a ) + a * 0xff ) / 0xff;
+        }
+    }
+}
+
+S7BMP *A7BmpCreateTab ( S7BMP *pTab, FT_Face face, CONST CHAR *pStr ) {
+
+    S7BMP *buf = A7BmpCreate ( D7BMP_A7_RGBA, 512, 512, 0 );
+    memset ( buf -> pData, 0, buf -> nHeight * buf -> nStride );
+
+    S7BMP *glyph = NULL;
+
+    UINT x = 0;
+    pTab -> nTop = 0;
+    pTab -> nLeft = 0;
+    A7BmpDrawAlphaMap ( buf, pTab, 0xff, 0x7f, 0x1f, 0xff, FALSE );
+    x += pTab -> nWidth * 64;
+    CONST UINT h = pTab -> nHeight * 32;
+    UINT x0 = pTab -> nWidth;
+    UINT x1 = 0;
+
+    for ( CHAR CONST *ch = pStr; *ch != 0; ++ch ) {
+        glyph = A7BmpLoad_Symbol ( glyph, face, *ch, h, x, h+h / 2 );
+        A7BmpDrawRect ( buf, x / 64, 0, glyph -> nAdvance / 64, pTab -> nHeight, 0xff, 0x7f, 0x1f, 0xff );
+        A7BmpDrawAlphaMap ( buf, glyph, 0x1f, 0x7f, 0xff, 0xff, FALSE );
+        x += glyph -> nAdvance;
+    }
+
+    pTab -> nTop = 0;
+    pTab -> nLeft = x / 64;
+    A7BmpDrawAlphaMap ( buf, pTab, 0xff, 0x7f, 0x1f, 0xff, TRUE );
+
+    if ( glyph ) A7BmpFree ( glyph );
+
+    return buf;
+}
+
 
 /* Хендл приложения */
 HINSTANCE       g_hInstance;
@@ -25,8 +98,20 @@ HWND            g_hWndMain;
 
 S7BMP           *g_bmpWallPapper;
 
-S7BMP           *g_bmpGDI_Layer0;
-S7BMP           *g_bmpGDI_Layer1;
+S7BMP           *g_bmpGDI_Layer_BackGround;
+S7BMP           *g_bmpGDI_Layer_Tabs;
+
+S7BMP           *g_bmp_Tab;
+
+S7BMP           *g_bmpTab;
+
+typedef struct _S7SETTINGS {
+
+} S7SETTINGS;
+
+
+FT_Library      g_ftLibrary;
+FT_Face         g_ftFace;
 
 /* Название процедуры главного окна */
 LRESULT CALLBACK A7MainWinProc ( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam ) {
@@ -46,21 +131,37 @@ LRESULT CALLBACK A7MainWinProc ( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lPa
         }
         case WM_CREATE: {
             g_bmpWallPapper = A7BmpCreateByJpegFileA ( "data/mountain_river_snow_winter_93245_1920x1080.jpg", FALSE );
-            g_bmpGDI_Layer0 = NULL;
+            g_bmp_Tab = A7BmpGen_Tab ( 32, 1.0f, 12.0f, 12.0f );
+            // g_bmp_Tab = A7BmpGen_Symbol ( g_ftFace, '@', 64*73, 64*73, 64*73 );
+
+            g_bmpTab = A7BmpCreateTab ( g_bmp_Tab, g_ftFace, "Hello World!" );
+
+
+            g_bmpGDI_Layer_BackGround = NULL;
+            g_bmpGDI_Layer_Tabs = NULL;
             return 0;
         }
         case WM_SIZE: {
             HDC hDC = GetDC ( hWnd );
-            if ( g_bmpGDI_Layer0 != NULL ) A7BmpFree ( g_bmpGDI_Layer0 );
-            g_bmpGDI_Layer0 = A7BmpCreateByGDI ( hDC, LOWORD ( lParam ), HIWORD ( lParam ), FALSE, FALSE );
-            A7BmpCopy ( g_bmpGDI_Layer0, 0, 0, g_bmpWallPapper, 0, 0, __min ( g_bmpGDI_Layer0 -> nWidth, g_bmpWallPapper -> nWidth ), __min ( g_bmpGDI_Layer0 -> nHeight, g_bmpWallPapper -> nHeight ) );
+            if ( g_bmpGDI_Layer_BackGround != NULL ) A7BmpFree ( g_bmpGDI_Layer_BackGround );
+            g_bmpGDI_Layer_BackGround = A7BmpCreateByGDI ( hDC, LOWORD ( lParam ), HIWORD ( lParam ), FALSE, FALSE );
+            A7BmpCopy ( g_bmpGDI_Layer_BackGround, 0, 0, g_bmpWallPapper, 0, 0, __min ( g_bmpGDI_Layer_BackGround -> nWidth, g_bmpWallPapper -> nWidth ), __min ( g_bmpGDI_Layer_BackGround -> nHeight, g_bmpWallPapper -> nHeight ) );
+
+            if ( g_bmpGDI_Layer_Tabs != NULL ) A7BmpFree ( g_bmpGDI_Layer_Tabs );
+            g_bmpGDI_Layer_Tabs = A7BmpCreateByGDI ( hDC, LOWORD ( lParam ), HIWORD ( lParam ), TRUE, FALSE );
+
+            A7BmpCopy ( g_bmpGDI_Layer_Tabs, 0, 0, g_bmpTab, 0, 0, __min ( g_bmpGDI_Layer_Tabs -> nWidth, g_bmpTab -> nWidth ), __min ( g_bmpGDI_Layer_Tabs -> nHeight, g_bmpTab -> nHeight ) );
+
             ReleaseDC ( hWnd, hDC );
             return 0;
         }
         case WM_DESTROY: {
             PostQuitMessage ( 0 );
+            A7BmpFree ( g_bmp_Tab );
             A7BmpFree ( g_bmpWallPapper );
-            if ( g_bmpGDI_Layer0 != NULL ) A7BmpFree ( g_bmpGDI_Layer0 );
+            A7BmpFree ( g_bmpTab );
+            if ( g_bmpGDI_Layer_BackGround != NULL ) A7BmpFree ( g_bmpGDI_Layer_BackGround );
+            if ( g_bmpGDI_Layer_Tabs != NULL ) A7BmpFree ( g_bmpGDI_Layer_Tabs );
             return 0;
         }
 
@@ -77,7 +178,8 @@ LRESULT CALLBACK A7MainWinProc ( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lPa
                 nCWidth = rt.right - rt.left;
                 nCHeight = rt.bottom - rt.top;
             }
-            A7BmpGDI_Draw ( hDC, 0, 0, g_bmpGDI_Layer0, 0, 0, nCWidth, nCHeight );
+            A7BmpGDI_Draw ( hDC, 0, 0, g_bmpGDI_Layer_BackGround, 0, 0, nCWidth, nCHeight );
+            A7BmpGDI_Draw ( hDC, 0, 0, g_bmpGDI_Layer_Tabs, 0, 0, nCWidth, nCHeight );
             EndPaint ( hWnd, &ps );
             // DeleteObject ( hbrBlack );
         }
@@ -89,6 +191,9 @@ LRESULT CALLBACK A7MainWinProc ( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lPa
 
 /* Точка входа в приложение */
 INT APIENTRY wWinMain ( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, INT nShowCmd ) {
+
+    FT_Init_FreeType ( &g_ftLibrary );
+    FT_New_Face ( g_ftLibrary, "C:\\Windows\\Fonts\\RobotoSlab-Regular.ttf", 0, &g_ftFace );
 
     g_hInstance = hInstance;
     /* Регистрация класса главного окна */
@@ -112,7 +217,7 @@ INT APIENTRY wWinMain ( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpC
     }
 
     /* Создание главного окна */
-    if ( ( g_hWndMain = CreateWindowExW ( 0, ( LPCWSTR ) g_iMainClassAtom, NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, g_hInstance, NULL ) ) == NULL )
+    if ( ( g_hWndMain = CreateWindowExW ( 0, ( ( LPCWSTR ) ( g_iMainClassAtom ) ), NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, g_hInstance, NULL ) ) == NULL )
         D7ERROREXIT ( CreateWindowExW );
 
     // ShowWindow ( g_hWndMain, nCmdShow );
@@ -130,6 +235,8 @@ INT APIENTRY wWinMain ( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpC
     if ( ! UnregisterClassW ( kg_szMainClassName, g_hInstance ) )
         D7ERROREXIT ( UnregisterClassW );
 
+    FT_Done_Face ( g_ftFace );
+    FT_Done_FreeType ( g_ftLibrary );
 
     return 0;
 }
