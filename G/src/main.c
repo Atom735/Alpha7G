ea233ce638a6698f505dc892f53f84591b1b8096
Atom735 <Atom735.ag47@gmail.com>
2019-07-03 04:51:20 +0300

    Create Window

--- G/src/main.c
+++ G/src/main.c
@@ -1,8 +1,65 @@
 #include <Windows.h>
 
+HINSTANCE g_hInstance;
+CONST LPCWSTR g_ksMainClassName = L"WCNA7_MAIN";
+
+/* Процедура главного окна */
+LRESULT CALLBACK rMsgProc ( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam ) {
+    switch ( uMsg ) {
+        case WM_CREATE: {
+            return 0;
+        }
+        case WM_SIZE: {
+            return 0;
+        }
+        case WM_DESTROY: {
+            PostQuitMessage ( 0 );
+            return 0;
+        }
+        case WM_MOUSEMOVE: {
+            return 0;
+        }
+        case WM_COMMAND: {
+            return 0;
+        }
+        case WM_PAINT: {
+            PAINTSTRUCT ps;
+            HDC hDC = BeginPaint ( hWnd, &ps );
+            FillRect ( hDC, &ps.rcPaint, GetSysColorBrush ( 1 ) );
+            EndPaint ( hWnd, &ps );
+            return 0;
+        }
+    }
+    return DefWindowProc ( hWnd, uMsg, wParam, lParam );
+}
+
+/* Точка входа в приложение */
 INT APIENTRY wWinMain ( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, INT nShowCmd ) {
-
     MSG msg = { };
-
+    /* Регистрация класса главного окна */
+    WNDCLASSEXW wc = {
+        .cbSize        = sizeof ( WNDCLASSEXW ),
+        .style         = CS_VREDRAW | CS_HREDRAW,
+        .lpfnWndProc   = rMsgProc,
+        .cbClsExtra    = 0,
+        .cbWndExtra    = 0,
+        .hInstance     = hInstance,
+        .hIcon         = NULL,
+        .hCursor       = NULL,
+        .hbrBackground = NULL,
+        .lpszMenuName  = NULL,
+        .lpszClassName = g_ksMainClassName,
+        .hIconSm       = NULL,
+    };
+    RegisterClassExW ( &wc );
+    /* Создание главного окна */
+    CreateWindowExW ( 0, g_ksMainClassName, NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL );
+    /* Входим в цикл обработки сообщений */
+    while ( GetMessage ( &msg, NULL, 0, 0 ) ) {
+        TranslateMessage ( &msg );
+        DispatchMessage ( &msg );
+    }
+    /* Освобождение класса главного окна */
+    UnregisterClassW ( g_ksMainClassName, hInstance );
     return (INT) msg.wParam;
 }
